18ab8514b37133bafb6f2b4abde115fc0fdba518
gskleres <george.skleres@gmail.com>
2016-10-25 10:27:40 -0400

    softer Status Message
    status message - no longer has big black box and text is softer and more
    compact

--- UI/Panels/StatusMessagePanel.lua
+++ UI/Panels/StatusMessagePanel.lua
@@ -1,3 +1,5 @@
+-- Modified by HellBlazer on 2016-10-24
+
 -- =========================================================================== 
 -- Status Message Manager
 -- Non-interactive messages that appear in the upper-center of the screen.
@@ -16,6 +18,9 @@ local DEFAULT_TIME_TO_DISPLAY	:number = 10;	-- Seconds to display the message
 
 local m_statusIM				:table = InstanceManager:new( "StatusMessageInstance", "Root", Controls.StackOfMessages );
 local m_gossipIM				:table = InstanceManager:new( "GossipMessageInstance", "Root", Controls.StackOfMessages );
+-- HB_FIX_WAR_MESSAGE_BLOCKING_SCREEN begin
+local m_connectionIM			:table = InstanceManager:new( "ConnectionMessageInstance", "Root", Controls.StackOfMessages);
+-- HB_FIX_WAR_MESSAGE_BLOCKING_SCREEN end
 
 local PlayerConnectedChatStr	:string = Locale.Lookup( "LOC_MP_PLAYER_CONNECTED_CHAT" );
 local PlayerDisconnectedChatStr :string	= Locale.Lookup( "LOC_MP_PLAYER_DISCONNECTED_CHAT" );
@@ -32,8 +37,12 @@ local m_kMessages :table = {};
 -- =========================================================================== 
 function OnStatusMessage( str:string, fDisplayTime:number, type:number )
 
-	if (type == ReportingStatusTypes.DEFAULT or
-		type == ReportingStatusTypes.GOSSIP) then	-- A type we handle?
+-- HB_FIX_WAR_MESSAGE_BLOCKING_SCREEN begin
+	if (type == 99 or type == ReportingStatusTypes.GOSSIP or type == ReportingStatusTypes.DEFAULT) then	-- A type we handle?
+-- HB_FIX_WAR_MESSAGE_BLOCKING_SCREEN old code
+--	if (type == ReportingStatusTypes.DEFAULT or
+--		type == ReportingStatusTypes.GOSSIP) then	-- A type we handle?
+-- HB_FIX_WAR_MESSAGE_BLOCKING_SCREEN end
 
 		local kTypeEntry :table = m_kMessages[type];
 		if (kTypeEntry == nil) then
@@ -47,10 +56,22 @@ function OnStatusMessage( str:string, fDisplayTime:number, type:number )
 			-- Link to the instance manager and the stack the UI displays in
 			if (type == ReportingStatusTypes.GOSSIP) then
 				kTypeEntry.InstanceManager	= m_gossipIM;
+				
+-- HB_FIX_WAR_MESSAGE_BLOCKING_SCREEN begin
+			elseif (type == 99) then
+				kTypeEntry.InstanceManager  = m_connectionIM;
+-- HB_FIX_WAR_MESSAGE_BLOCKING_SCREEN end
+				
 			else
 				kTypeEntry.InstanceManager	= m_statusIM;
 			end
 		end
+
+-- HB_FIX_WAR_MESSAGE_BLOCKING_SCREEN begin
+		if ( type == ReportingStatusTypes.DEFAULT ) then
+			fDisplayTime = 3;
+		end
+-- HB_FIX_WAR_MESSAGE_BLOCKING_SCREEN end
 
 		local pInstance:table = kTypeEntry.InstanceManager:GetInstance();
 		table.insert( kTypeEntry.MessageInstances, pInstance );
@@ -81,7 +102,11 @@ function OnMultplayerPlayerConnected( playerID )
 	if( ContextPtr:IsHidden() == false and GameConfiguration.IsNetworkMultiplayer() ) then
 		local pPlayerConfig = PlayerConfigurations[playerID];
 		local statusMessage = pPlayerConfig:GetPlayerName() .. " " .. PlayerConnectedChatStr;
-		OnStatusMessage( statusMessage, DEFAULT_TIME_TO_DISPLAY, ReportingStatusTypes.DEFAULT );
+-- HB_FIX_WAR_MESSAGE_BLOCKING_SCREEN begin
+		OnStatusMessage( statusMessage, DEFAULT_TIME_TO_DISPLAY, 99 );
+-- HB_FIX_WAR_MESSAGE_BLOCKING_SCREEN old code
+--		OnStatusMessage( statusMessage, DEFAULT_TIME_TO_DISPLAY, ReportingStatusTypes.DEFAULT );
+-- HB_FIX_WAR_MESSAGE_BLOCKING_SCREEN end
 	end
 end
 
@@ -95,7 +120,11 @@ function OnMultiplayerPrePlayerDisconnected( playerID )
 		else
     		statusMessage = statusMessage .. " " .. PlayerDisconnectedChatStr;
 		end
-		OnStatusMessage(statusMessage, DEFAULT_TIME_TO_DISPLAY, ReportingStatusTypes.DEFAULT);
+-- HB_FIX_WAR_MESSAGE_BLOCKING_SCREEN begin
+		OnStatusMessage(statusMessage, DEFAULT_TIME_TO_DISPLAY, 99);
+-- HB_FIX_WAR_MESSAGE_BLOCKING_SCREEN old code
+--		OnStatusMessage(statusMessage, DEFAULT_TIME_TO_DISPLAY, ReportingStatusTypes.DEFAULT);
+-- HB_FIX_WAR_MESSAGE_BLOCKING_SCREEN end
 	end
 end
 
